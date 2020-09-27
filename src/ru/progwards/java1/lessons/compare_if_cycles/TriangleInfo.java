a819c470bd205e652ec856c28a58d1134f7675f1
Student <sanek-hardin@yandex.ru>
2020-09-27 10:42:06 +0500

    Hello

--- src/ru/progwards/java1/lessons/compare_if_cycles/TriangleInfo.java
+++ src/ru/progwards/java1/lessons/compare_if_cycles/TriangleInfo.java
@@ -7,25 +7,24 @@ public class TriangleInfo {
         return result;
     }
     public static boolean isRightTriangle(int a, int b, int c){
-        boolean result = ((c * c) == ((a * a) + (b * b))) && ((a * a) == ((b * b) + (c * c))) && ((b * b) == ((a * a) + (c * c)));{
+        boolean result = (c * c) == ((a * a) + (b * b)); {
             System.out.println(result);
             return result;
         }
     }
     public static boolean isIsoscelesTriangle(int a, int b, int c){
-        boolean result = false;
-        if (a == b)
-        System.out.println(true);
-        else if (b == c)
+        if (a == b) {
+            System.out.println(true);
+        } else if (b == c)
             System.out.println(true);
         else if (c == a)
             System.out.println(true);
-        return result;
+        return true;
             }
     public static void main(String[] args) {
         isTriangle(50, 10, 43);
         isRightTriangle(3, 4, 5 );
-        isIsoscelesTriangle(7, 7, 5);
+        isIsoscelesTriangle(5, 5, 5);
 
     }
 
--- src/ru/progwards/java1/lessons/compare_if_cycles/TriangleSimpleInfo.java
+++ src/ru/progwards/java1/lessons/compare_if_cycles/TriangleSimpleInfo.java
@@ -3,22 +3,22 @@ package ru.progwards.java1.lessons.compare_if_cycles;
 public class TriangleSimpleInfo {
       public static int maxSide(int a, int b, int c){
         if ((a > (b + c)) && (b > (a + c)) && (c > (b + a)));
-        else if (a > b && a > c){
+        else if (a >= b && a >= c){
             return a; }
-        else if (b > c && b > a){
+        else if (b >= c && b >= a){
             return b;}
-        else if (c > a && c > b){
+        else if (c >= a && c >= b){
         return c;
         }
         return 0;}
     public static int minSide(int a, int b, int c) {
         if ((a > (b + c)) && (b > (a + c)) && (c > (b + a))) ;
-        else if (a < c && a < b){
+        else if (a <= c && a <= b){
             return a;}
 
-        else if (b < c && b < a){
+        else if (b <= c && b <= a){
             return b;}
-        else if (c < a && c < b){
+        else if (c <= a && c <= b){
             return c;}
         return 0;
     }
