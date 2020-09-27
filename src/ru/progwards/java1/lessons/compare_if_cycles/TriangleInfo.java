d97906ebb01ebc6d8eb013b8da3041a1cacfce20
Student <sanek-hardin@yandex.ru>
2020-09-27 15:48:57 +0500

    Hello

--- src/ru/progwards/java1/lessons/compare_if_cycles/TriangleInfo.java
+++ src/ru/progwards/java1/lessons/compare_if_cycles/TriangleInfo.java
@@ -7,24 +7,36 @@ public class TriangleInfo {
         return result;
     }
     public static boolean isRightTriangle(int a, int b, int c){
-        boolean result = (c * c) == ((a * a) + (b * b)); {
-            System.out.println(result);
-            return result;
-        }
+        boolean result = true;
+         if ((c * c) == ((a * a) + (b * b))){
+            System.out.println(true);
+         }else if ((a * a) == ((c * c) + (b * b))){
+             System.out.println(true);
+         }else if ((b * b) == ((a * a) + (c * c))){
+             System.out.println(true);
+         }
+        return result;
     }
+
     public static boolean isIsoscelesTriangle(int a, int b, int c){
-        if (a == b) {
+        boolean result = true;
+        if (a == b){
             System.out.println(true);
-        } else if (b == c)
+             }
+        else if (b == c){
             System.out.println(true);
-        else if (c == a)
-            System.out.println(true);
-        return true;
-            }
+        }
+        else if (c == a){
+            System.out.println(true); }
+        else if (a != b && b != c){
+            System.out.println(false);
+        }
+        return result;
+    }
     public static void main(String[] args) {
         isTriangle(50, 10, 43);
-        isRightTriangle(3, 4, 5 );
-        isIsoscelesTriangle(5, 5, 5);
+        isRightTriangle(4, 5, 3 );
+        isIsoscelesTriangle(5, 2, 4);
 
     }
 
--- src/ru/progwards/java1/lessons/compare_if_cycles/TriangleSimpleInfo.java
+++ src/ru/progwards/java1/lessons/compare_if_cycles/TriangleSimpleInfo.java
@@ -3,24 +3,15 @@ package ru.progwards.java1.lessons.compare_if_cycles;
 public class TriangleSimpleInfo {
       public static int maxSide(int a, int b, int c){
         if ((a > (b + c)) && (b > (a + c)) && (c > (b + a)));
-        else if (a >= b && a >= c){
-            return a; }
-        else if (b >= c && b >= a){
-            return b;}
-        else if (c >= a && c >= b){
-        return c;
-        }
-        return 0;}
+        else if (a >= b && a >= c) return a;
+        else if (b >= c && b >= a) return b;
+        else if (c >= a && c >= b) return c; return 0;
+      }
     public static int minSide(int a, int b, int c) {
         if ((a > (b + c)) && (b > (a + c)) && (c > (b + a))) ;
-        else if (a <= c && a <= b){
-            return a;}
-
-        else if (b <= c && b <= a){
-            return b;}
-        else if (c <= a && c <= b){
-            return c;}
-        return 0;
+        else if (a <= c && a <= b) return a;
+        else if (b <= c && b <= a) return b;
+        else if (c <= a && c <= b) return c; return 0;
     }
     public static boolean isEquilateralTriangle(int a, int b, int c){
           boolean result = a == b && b == c;
